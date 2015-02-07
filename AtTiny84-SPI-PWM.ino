f1c6b198ecac62e77b7e4e7715f08b865f263860
jnj16180340 <jnj.16180340@gmail.com>
2015-02-07 05:19:17 -0500

    Began port to At84, PWM works other stuff untested
    readme is in markdown

--- AtTiny84-SPI-PWM.ino
+++ AtTiny84-SPI-PWM.ino
@@ -1,18 +1,34 @@
+// ATMEL ATTINY84 / ARDUINO
+//
+//                           +-\/-+
+//                     VCC  1|    |14  GND
+//             (D  0)  PB0  2|    |13  AREF (D 10)
+//             (D  1)  PB1  3|    |12  PA1  (D  9)
+//                     PB3  4|    |11  PA2  (D  8)
+//  PWM  INT0  (D  2)  PB2  5|    |10  PA3  (D  7)
+//  PWM        (D  3)  PA7  6|    |9   PA4  (D  6)
+//  PWM        (D  4)  PA6  7|    |8   PA5  (D  5)        PWM
+//                           +----+
+
+
 #include <stdlib.h>
-//#include <EEPROM.h>
+//#include <EEPROM.h> //this would be for storing settings if we had any
 #include <avr/interrupt.h>
 #include <avr/io.h>        // Adds useful constants
 #include <util/delay.h>    // Adds delay_ms and delay_us functions
 
 // SPI
-#define MOSI 0	//physical pin 5
-#define MISO 1	//physical pin 6
-#define SCK  2	//physical pin 7
-#define CS   3	//physical pin 2
-#define PCIE0  5	// Pin change interrupts
+#define MOSI 4	//84:pp7  | 85:pp5 //somebody else switches mosi,miso pin values
+#define MISO 5	//84:pp8  | 85:pp6
+#define SCK  6	//84:pp9  | 85:pp7
+#define CS   7	//84:pp10, PA3 | 85:pp2
+//#define PCIE0  5	// Pin change interrupts //necessary???
 
 // PWM
-#define PWMA 4
+#define PWMA 2 //84:pp5
+#define PWMB 3 //84:pp6
+#define ADCA 8 //84: pp11
+#define ADCB 9 //84:pp12
 
 #define F_CPU 8000000	// This is used by delay.h library
 
@@ -30,6 +46,7 @@ void setup() {
 }
 
 void setupSPI(){
+  // This should be complete.
   //pin setup
   pinMode(MISO, INPUT); //disabled for start condition
   pinMode(MOSI, INPUT);
@@ -38,22 +55,32 @@ void setupSPI(){
 
   //actual USI setup. Important... match the master settings accordingly
   USICR = 0; //set everything to zero.
-  USICR = (1 << USIWM0) | (1 << USICS1) | (1 << USIOIE); //Three wire mode0, External clock positive edge both edges, Enable interrupt
+  USICR = (1 << USIWM0) | (1 << USICS1) | (1 << USIOIE); //Three wire mode0, External clock positive edge both edges, Enable counter overflow interrupt
 
   //chip select interrupt
-  PCMSK |= (1<<PCINT3); //PB3 Pin change interrupt PCINT1, which is handled by ISR for PCINT0_vect
+  // 84: Becomes PCMSK1 + PCMSK0 (xxxx11,pcint10,9,8 + pcint7,...,0)
+  // 84: So pp10 is still PCINT3... woot
+  PCMSK0 |= (1<<PCINT3); //PB3 Pin change interrupt PCINT1, which is handled by ISR for PCINT0_vect
   //PCMSK |= 0b00001000;
-  GIMSK  |= (1<<PCIE0);
+  GIMSK  |= (1<<PCIE0); // 84: PCIE1 is also available.
   //GIMSK |= 0b00100000;
   sei();
 }
 
 void setupPwm(){
   // PWM setup
+  // NB OC1x provides 16 bit pwm, but is used for the USI...
   pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
   // Configure counter/timer1 for fast PWM on PB4
   // COM1B0: 2 -> normal, 3 -> inverting
-  GTCCR = 1<<PWM1B | 2<<COM1B0;
+  //85: Enable PWMB | Set COM1(B1,B0) to (1,0) for OC1x active, ~OC1x not connected
+  //GTCCR = 1<<PWM1B | 2<<COM1B0; //84: GTCCR does less/different stuff??
+  
+  // set normal mode for OC0A,B
+  TCCR0A = 1<<COM0A1 | 1<<COM0B1 | 1<<WGM01 | 1<<WGM00; //WGM01,WGM00
+  // TCCR1: : CS02,01,00 (0,0,1)=fclk to (1,0,1) =fclk/1024, (1,1,0) = external clk
+  TCCR1A = 1<<CS00; //set 1<<WGM02 for 'mode 7' fast pwm, top less than 0xFF see  p83//CS02,01,00 are clock select bits, see p84
   //TCCR1 = 3<<COM1A0 | 7<<CS10;
   // CS10 is a clock rate divisor; COM1A0 set above is bugfix which is irrelevant (we're not using inverted mode, which needs COM1A0 to be nonzero)
   // but scope (crappy computer scope) says 62500ish...
@@ -63,7 +90,7 @@ void setupPwm(){
   // 4: 4000
   // 7: 500
   // 8: 250
-  TCCR1 = 1<<CS10; // sets PWM frequency to about 40kHz??? if we believe the scope
+  //TCCR1 = 1<<CS10; // sets PWM frequency to about 40kHz??? if we believe the scope, see 85:p89
   
   SetPwm(0);
   
@@ -71,13 +98,24 @@ void setupPwm(){
 
 // Sets PWM on pin 4
 inline void SetPwm (int duty) {
-  OCR1B = duty;
+  OCR0A = duty;
+  OCR0B = duty;
 }
 
 void loop() {
   if(recvFlag){
     SetPwm(recvByte);
     recvFlag = false;
+  }
+  
+  //test code delete me
+  for(int i = 0; i<0xFF; i++){
+   //It seems to get reeaal wonky if both PWM pins draw too much current
+   //But not always!!!
+   OCR0A = i;
+   OCR0B = 0xFF-i;
+   //SetPwm(i);
+   delay(20); 
   }
 }
 
--- README.md
+++ README.md
@@ -1,34 +1,38 @@
-# AtTiny85-SPI-PWM
-/*
+# AtTiny84-SPI-PWM  
+===
+ This is in progress, porting to AtTiny84 and adding functionality. Currently (pun intended), 
  
- AtTiny85 acts as SPI slave (Mode 0); upon receiving a byte it sets fast PWM duty cycle to that byte. Currently the PWM frequency is hardcoded.
+ **PWMs** are tested, kind of
+ **SPI** is untested
+ **ADCs** aren't added yet  
  
- This uses the AtTiny85 Arduino core available on code.google.com
+ **TODO: Port to AtTiny84 and add features** 
+ *PWM: Investigate whether fast mode is outputting 1/255 duty cycle when set to 0x00 and change if need be
+ *SPI: Port it
+ *SPI: 2 byte input
+ *SPI: Commands, such as read-without-setting and change-clock-speed
+ *SPI: Output
+ *ADC: read
+ *Meta: Figure out a sane way to structure repos AND be able to use the arduino ide OR switch to something a little more rational  
+ *MCU: Store values (frequency etc.) in EEPROM???
+ *MCU: Add watchdog timer etc?
+ *MCU: *Robust* protection against activating both PWM at the same time... set option in eeprom in case we aren't using a full bridge in the future  
  
- TODO:
-   Store values (frequency etc.) in EEPROM
-   Multiplex SPI pins to free up another pin for more PWM???? NO, it SUCKS.
-   Multiplex MISO pin for analog read?? POSSIBLE.
-   SPI:
-     Multi bytes OR less than 8 bit resolution
-     Clock prescaler and wider range of frequencies
-     H-bridge control... Is it possible??? NO.
+ AtTiny84 acts as SPI slave upon receiving a byte it sets fast PWM duty cycle to that byte. Currently the PWM frequency is hardcoded.  
  
- ATtiny85 test code
+ Master SPI settings: Mode 0, MSB first, 1mHz = SPI_CLOCK_DIV16. Sending BLAH 
  
- The connections to the ATTiny are as follows:
- ATTiny    Arduino    Info
- Pin  1  - 5          RESET / Rx (Not receiving any data)
- Pin  2  - 3          CS (chip select fot spi)
- Pin  3  - 4          PWM output
- Pin  4  -            GND
- Pin  5  - 0          MOSI
- Pin  6  - 1          MISO
- Pin  7  - 2 / A1     SCK
- Pin  8  -   +Vcc
+ This uses the AtTiny85 Arduino core available on code.google.com [https://code.google.com/p/arduino-tiny/source/browse/avr/cores/tiny/pins_arduino.c?repo=core1-0150#164]  
  
- SEE:
- http://www.technoblogy.com/show?LE0
- http://www.technoblogy.com/show?KVO
+  // ATMEL ATTINY84 / ARDUINO
+  //
+  //                           +-\/-+
+  //                     VCC  1|    |14  GND
+  //             (D  0)  PB0  2|    |13  AREF (D 10)
+  //             (D  1)  PB1  3|    |12  PA1  (D  9)
+  //                     PB3  4|    |11  PA2  (D  8)
+  //  PWM  INT0  (D  2)  PB2  5|    |10  PA3  (D  7)
+  //  PWM        (D  3)  PA7  6|    |9   PA4  (D  6)
+  //  PWM        (D  4)  PA6  7|    |8   PA5  (D  5)        PWM
+  //                           +----+
 
- */
